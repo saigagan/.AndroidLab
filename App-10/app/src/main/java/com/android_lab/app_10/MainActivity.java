75da849601d75f181eaaa46d99606f0651d5594a
SaiHemanthBR <sh.bheemreddy@icloud.com>
2019-07-27 22:12:45 +0530

    Completed App-10

--- App-10/app/src/main/java/com/android_lab/app_10/MainActivity.java
+++ App-10/app/src/main/java/com/android_lab/app_10/MainActivity.java
@@ -2,41 +2,66 @@ package com.android_lab.app_10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private Spinner foreground, background;
     private TextView textView;
+
     private int[] options = {R.color.white, R.color.black, R.color.gray, R.color.red, R.color.green, R.color.blue};
+
+    private final String MyPrefs = "MyPrefs";
+    private final String FColor = "ForegroundColor";
+    private final String BColor = "BackgroundColor";
+    private SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sp = getSharedPreferences(MyPrefs, Context.MODE_PRIVATE);
+
         textView = findViewById(R.id.textView);
         foreground = findViewById(R.id.foreground);
-        foreground.setOnItemSelectedListener(this);
         background = findViewById(R.id.background);
-        background.setOnItemSelectedListener(this);
-    }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        switch(view.getId()) {
-            case R.id.foreground:
-                textView.setTextColor(options[i]);
-                break;
-            case R.id.background:
+        foreground.setSelection(sp.getInt(FColor, 1));
+        background.setSelection(sp.getInt(BColor, 0));
+
+        foreground.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                textView.setTextColor(getResources().getColor(options[i]));
+
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putInt(FColor, i);
+                editor.commit();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) { }
+        });
+
+        background.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 findViewById(android.R.id.content).setBackgroundResource(options[i]);
-                break;
-        }
-    }
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putInt(BColor, i);
+                editor.commit();
+            }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {}
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
+    }
 }
